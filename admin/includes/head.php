c01bee2a31da17041c35c50145a015eed4803648
Rajan Sharma Mac <rajankumarsharma681@gmail.com>
2025-07-13 23:53:34 +0530

    remove ck edito cdn

--- admin/includes/head.php
+++ admin/includes/head.php
@@ -21,7 +21,6 @@ if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== tru
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/46.0.0/ckeditor5.css" /> -->
 </head>
 
 <body class="sb-nav-fixed">
